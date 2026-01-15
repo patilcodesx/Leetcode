a375c91f9346b12e421e3695cdf1c3e6eec5ef94
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-01-15 10:34:27 +0530

    Time: 34 ms (57.93%), Space: 59.1 MB (65.09%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -20,7 +20,7 @@ class Solution {
 
                     //skip duplicates
                     while(left<right && nums[left]==nums[left+1]) left++;
-                    while(left<right && nums[right]==nums[right-1]) right++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
 
 
                     left++;
