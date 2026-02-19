e4c326362a2c6a308703d2810c81d141158cae07
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-02-19 10:20:45 +0530

    Time: 1098 ms (5.05%), Space: 49.7 MB (75.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,35 +2,35 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
 
-        int[] flags = new int[strs.length];
+        int[] flag = new int[strs.length];
 
-       for(int i=0;i<strs.length;i++){
-          if(flags[i]==1) continue;
+        for(int i=0;i<strs.length;i++){
+            List<String> list = new ArrayList<>();
+            list.add(strs[i]);
+              if(flag[i]==1) continue;
 
-          List<String> list = new ArrayList<>();
-          list.add(strs[i]);
-          flags[i] = 1;
-
-        for(int j=i+1;j<strs.length;j++){
-               if(flags[j]==0 && isAnagram(strs[i],strs[j])){
-                list.add(strs[j]);
-                flags[j] =1;
+            for(int j=i+1;j<strs.length;j++){
+               if(flag[j]==0 && isAnagrams(strs[i],strs[j])){
+                   list.add(strs[j]);
+                   flag[j] =1;
                }
-          }
-          result.add(list);
-       }
-       return result;
+            }
+            result.add(list);
+        }
+        return result;
     }
 
-    private boolean isAnagram(String s1, String s2){
-        if(s1.length()!= s2.length()) return false;
+    private boolean isAnagrams(String a, String b){
+         if(a.length() != b.length()){
+            return false;
+         }
 
-        char[] a = s1.toCharArray();
-        char[] b = s2.toCharArray();
+           char[] a1 = a.toCharArray();
+           char[] b1 = b.toCharArray();
 
-        Arrays.sort(a);
-        Arrays.sort(b);
+         Arrays.sort(a1);
+         Arrays.sort(b1);
 
-        return Arrays.equals(a,b);
+         return Arrays.equals(a1,b1);
     }
 }
