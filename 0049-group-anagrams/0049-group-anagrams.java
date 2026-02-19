6d122077ab93104e93f71cbb810e578820dae72c
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-02-19 10:30:31 +0530

    Time: 6 ms (99.37%), Space: 49.6 MB (85.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,54 @@
 class Solution {
+
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
+      Map<String,List<String>> map = new HashMap<>();
 
-        int[] flag = new int[strs.length];
+      for(String word: strs){
+        char[] strarr = word.toCharArray();
+        Arrays.sort(strarr);
 
-        for(int i=0;i<strs.length;i++){
-            List<String> list = new ArrayList<>();
-            list.add(strs[i]);
-              if(flag[i]==1) continue;
+        String key = new String(strarr);
+        map.putIfAbsent(key,new ArrayList<>());
+        map.get(key).add(word);
+      }
+      return new ArrayList<>(map.values());
 
-            for(int j=i+1;j<strs.length;j++){
-               if(flag[j]==0 && isAnagrams(strs[i],strs[j])){
-                   list.add(strs[j]);
-                   flag[j] =1;
-               }
-            }
-            result.add(list);
-        }
-        return result;
     }
 
-    private boolean isAnagrams(String a, String b){
-         if(a.length() != b.length()){
-            return false;
-         }
 
-           char[] a1 = a.toCharArray();
-           char[] b1 = b.toCharArray();
 
-         Arrays.sort(a1);
-         Arrays.sort(b1);
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     List<List<String>> result = new ArrayList<>();
 
-         return Arrays.equals(a1,b1);
-    }
+    //     int[] flag = new int[strs.length];
+
+    //     for(int i=0;i<strs.length;i++){
+    //         List<String> list = new ArrayList<>();
+    //         list.add(strs[i]);
+    //           if(flag[i]==1) continue;
+
+    //         for(int j=i+1;j<strs.length;j++){
+    //            if(flag[j]==0 && isAnagrams(strs[i],strs[j])){
+    //                list.add(strs[j]);
+    //                flag[j] =1;
+    //            }
+    //         }
+    //         result.add(list);
+    //     }
+    //     return result;
+    // }
+
+    // private boolean isAnagrams(String a, String b){
+    //      if(a.length() != b.length()){
+    //         return false;
+    //      }
+
+    //        char[] a1 = a.toCharArray();
+    //        char[] b1 = b.toCharArray();
+
+    //      Arrays.sort(a1);
+    //      Arrays.sort(b1);
+
+    //      return Arrays.equals(a1,b1);
+    // }
 }
