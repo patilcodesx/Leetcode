54d6bae5abf5d811ad3c0b9adea2650481704909
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-02-25 10:42:43 +0530

    Time: 7 ms (78.21%), Space: 46.9 MB (27.94%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,20 +1,41 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        
+        Integer[] nums = new Integer[arr.length];
 
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                int a = Integer.bitCount(arr[i]);
-                int b = Integer.bitCount(arr[j]);
+            nums[i] = arr[i];
+        } 
 
-                if(b<a || (b==a && arr[j]<arr[i])){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        Arrays.sort(nums,(a,b)->{
+            int bitA = Integer.bitCount(a);
+            int bitB = Integer.bitCount(b);
+
+            if(bitA == bitB){
+               return a-b;
             }
+
+            return bitA-bitB;
+        });
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = nums[i];
         }
 
         return arr;
+
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         int a = Integer.bitCount(arr[i]);
+        //         int b = Integer.bitCount(arr[j]);
+
+        //         if(b<a || (b==a && arr[j]<arr[i])){
+        //             int temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+
+        // return arr;
     }
 }
