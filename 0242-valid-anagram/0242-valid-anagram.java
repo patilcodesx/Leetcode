3ec3a022c809fcf455175cad3bfabda0eebc1632
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-03-04 15:47:15 +0530

    Time: 14 ms (26.17%), Space: 46.5 MB (41.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,33 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-         int[] freq = new int[26];
-        for(int i=0;i<s.length();i++){
-             freq[s.charAt(i)-'a']++;
-             freq[t.charAt(i)-'a']--;
+        //  int[] freq = new int[26];
+        // for(int i=0;i<s.length();i++){
+        //      freq[s.charAt(i)-'a']++;
+        //      freq[t.charAt(i)-'a']--;
+        // }
+
+        // for(int i=0;i<s.length();i++){
+        //     if(freq[s.charAt(i)-'a']!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        Map<Character,Integer> map =  new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
 
-        for(int i=0;i<s.length();i++){
-            if(freq[s.charAt(i)-'a']!=0){
+
+         for(char c: t.toCharArray()){
+            if(!map.containsKey(c)){
                 return false;
             }
-        }
-        return true;
+            map.put(c,map.get(c)-1);
+            if(map.get(c) < 0) return false;
+         }
+
+         return true;
     }
 }
