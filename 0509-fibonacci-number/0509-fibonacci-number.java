cc7a09e99a1bc0939be3e5487a6f06c8066d8e34
Bhavesh Patil <150371273+patilcodesx@users.noreply.github.com>
2026-03-05 09:44:48 +0530

    Time: 0 ms (100%), Space: 42 MB (69.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,33 @@
 class Solution {
+    HashMap<Integer,Integer> memo = new HashMap<>();
     public int fib(int n) {
         //Base case
         if(n==0) return 0;
         if(n==1) return 1;
 
-        //Recursive call
-        return fib(n-1) + fib(n-2);
+        // //Recursive call
+        // return fib(n-1) + fib(n-2);
+
+        // //Memoization
+        // if(memo.containsKey(n)) return memo.get(n);
+
+        // int result = fib(n-1) + fib(n-2);
+        // memo.put(n,result);
+
+        // return result;
+
+
+        int prev2 = 0 ;//F(n-2)
+        int prev1 = 1; //F(n-1)
+        int current = 0;
+
+        for(int i=2;i<=n;i++){
+            current = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return current;
+
     }
 }
